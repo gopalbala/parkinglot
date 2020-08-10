63bf131ff67c6e5499615ed94bae120294d1dec3
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-10 23:05:27 +0530

    Added payment related classes

---
+++ src/main/java/com/gb/parkinglot/model/parking/Payment.java
@@ -0,0 +1,30 @@
+package com.gb.parkinglot.model.parking;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class Payment {
+    private String id;
+    private String ticketId;
+    private double amount;
+
+    @Setter
+    private LocalDateTime initiatedDate;
+    @Setter
+    private LocalDateTime completedDate;
+    @Setter
+    private PaymentStatus paymentStatus;
+
+    public Payment(String id, String ticketId, double amount) {
+        this.id = id;
+        this.ticketId = ticketId;
+        this.amount = amount;
+    }
+
+    public PaymentStatus initiatePayment() {
+        return null;
+    }
+}
