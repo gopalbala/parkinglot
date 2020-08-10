3082fd1dbc7ff21ee0b644af0aec14d0dd66df4c
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-10 23:11:30 +0530

    updated Admin

---
+++ src/main/java/com/gb/parkinglot/model/account/Admin.java
@@ -0,0 +1,15 @@
+package com.gb.parkinglot.model.account;
+
+import com.gb.parkinglot.model.parking.ParkingFloor;
+import com.gb.parkinglot.model.parking.ParkingLot;
+import com.gb.parkinglot.model.parking.ParkingSpot;
+
+public class Admin extends Account {
+    void addParkingFloor(ParkingLot parkingLot, ParkingFloor parkingFloor) {
+
+    }
+
+    void addParkingSpot(ParkingFloor parkingFloor, ParkingSpot parkingSpot) {
+
+    }
+}
