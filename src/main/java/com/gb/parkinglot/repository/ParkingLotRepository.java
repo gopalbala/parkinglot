076e979f2ad99e4f0253d48467383562b83a8c1f
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-11 23:51:11 +0530

    added paring spot

--- src/main/java/com/gb/parkinglot/repository/ParkingLotRepository.java
+++ src/main/java/com/gb/parkinglot/repository/ParkingLotRepository.java
@@ -1,8 +1,10 @@
 package com.gb.parkinglot.repository;
 
 import com.gb.parkinglot.exceptions.InvalidParkingLotException;
+import com.gb.parkinglot.exceptions.InvlaidParkingFloor;
 import com.gb.parkinglot.model.parking.ParkingFloor;
 import com.gb.parkinglot.model.parking.ParkingLot;
+import com.gb.parkinglot.model.parking.ParkingSpot;
 
 import java.util.*;
 
@@ -36,5 +38,25 @@ public class ParkingLotRepository {
 
         parkingLot.getParkingFloors().add(parkingFloor);
         return parkingFloor;
+    }
+
+    public ParkingSpot addParkingSpot(String parkingLotId, String parkingFloorId, ParkingSpot parkingSpot) throws InvalidParkingLotException, InvlaidParkingFloor {
+        ParkingLot parkingLot = parkingLotMap.get(parkingLotId);
+        if (parkingLot == null)
+            throw new InvalidParkingLotException("Invalid parking lot");
+        Optional<ParkingFloor> floor = parkingLot.getParkingFloors().stream()
+                .filter(pFloor -> pFloor.getFloorId()
+                        .equalsIgnoreCase(parkingFloorId)).findFirst();
+        if (!floor.isPresent()) {
+            throw new InvlaidParkingFloor("Invalid parking floor");
+        }
+        Optional<ParkingSpot> spot =
+                floor.get().getParkingSpots().stream().filter(pSpot ->
+                        pSpot.getParkingSpotId().equalsIgnoreCase(parkingSpot.getParkingSpotId())).findFirst();
+        if (spot.isPresent())
+            return spot.get();
+        floor.get().getParkingSpots().add(parkingSpot);
+        return parkingSpot;
+
     }
 }
