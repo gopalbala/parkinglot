ac0ef229d28bc0ca960358198f4e4324e553a5e2
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-13 02:41:49 +0530

    updations to parking floor and ParkingLot

--- src/main/java/com/gb/parkinglot/model/parking/ParkingFloor.java
+++ src/main/java/com/gb/parkinglot/model/parking/ParkingFloor.java
@@ -1,15 +1,87 @@
 package com.gb.parkinglot.model.parking;
 
+import com.gb.parkinglot.model.vehicle.Vehicle;
+import com.gb.parkinglot.model.vehicle.VehicleType;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-@Getter
-@Setter
+import static com.gb.parkinglot.model.parking.ParkingSpotType.*;
+
+
 public class ParkingFloor {
+    @Getter
+    @Setter
     private String floorId;
-    private Map<ParkingSpotType, List<ParkingSpot>> parkingSpots = new HashMap<>();
+    @Getter
+    @Setter
+    private Map<ParkingSpotType, TreeSet<ParkingSpot>> parkingSpots = new HashMap<>();
+    private Map<ParkingSpotType, TreeSet<ParkingSpot>> usedParkingSpots = new HashMap<>();
+
+    public ParkingFloor() {
+        parkingSpots.put(HANDICAPPED, getComparableTreeSet());
+        parkingSpots.put(CAR, getComparableTreeSet());
+        parkingSpots.put(LARGE, getComparableTreeSet());
+        parkingSpots.put(MOTORBIKE, getComparableTreeSet());
+        parkingSpots.put(ELECTRIC, getComparableTreeSet());
+    }
+
+    private TreeSet getComparableTreeSet() {
+        return new TreeSet<>(new Comparator<ParkingSpot>() {
+            @Override
+            public int compare(ParkingSpot o1, ParkingSpot o2) {
+                return o1.getParkingSpotId().compareTo(o2.getParkingSpotId());
+            }
+        });
+    }
+
+    public boolean isFloorFull() {
+        BitSet fullBitSet = new BitSet();
+        int bitIndex = 0;
+        for (Map.Entry<ParkingSpotType, TreeSet<ParkingSpot>> entry : parkingSpots.entrySet()) {
+            if (entry.getValue().size() == 0) {
+                fullBitSet.set(bitIndex++);
+            } else {
+                break;
+            }
+        }
+        return fullBitSet.cardinality() == fullBitSet.size();
+    }
+
+    public boolean canPark(VehicleType vehicleType, boolean isHandicapped) {
+        if (isHandicapped)
+            return parkingSpots.get(HANDICAPPED).size() > 0;
+        return canPark(getSpotTypeForVehicle(vehicleType));
+    }
+
+    public ParkingSpot getSpot(Vehicle vehicle, boolean handicapped) {
+        if (!canPark(vehicle.getType(), handicapped))
+            return null;
+
+        ParkingSpotType parkingSpotType = getSpotTypeForVehicle(vehicle.getType());
+        ParkingSpot parkingSpot = parkingSpots.get(parkingSpotType)
+                .first();
+        parkingSpots.remove(parkingSpot.getParkingSpotId());
+        usedParkingSpots.get(parkingSpotType).add(parkingSpot);
+        return parkingSpot;
+    }
+
+    private ParkingSpotType getSpotTypeForVehicle(VehicleType vehicleType) {
+        switch (vehicleType) {
+            case CAR:
+                return CAR;
+            case ELECTRIC:
+                return ELECTRIC;
+            case MOTORBIKE:
+                return MOTORBIKE;
+            default:
+                return LARGE;
+        }
+    }
+
+    private boolean canPark(ParkingSpotType parkingSpotType) {
+        return parkingSpots.get(parkingSpotType).size() > 0;
+    }
+
 }
--- src/main/java/com/gb/parkinglot/model/parking/ParkingLot.java
+++ src/main/java/com/gb/parkinglot/model/parking/ParkingLot.java
@@ -1,9 +1,11 @@
 package com.gb.parkinglot.model.parking;
 
 import com.gb.parkinglot.model.account.common.Address;
+import com.gb.parkinglot.model.vehicle.VehicleType;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.BitSet;
 import java.util.List;
 
 @Getter
@@ -14,4 +16,28 @@ public class ParkingLot {
     private List<ParkingFloor> parkingFloors;
     private List<EntrancePanel> entrancePanels;
     private List<ExitPanel> exitPanels;
+
+    public static ParkingLot INSTANCE = new ParkingLot();
+
+    private ParkingLot() {
+
+    }
+
+    public boolean isFull() {
+        int index = 0;
+        BitSet bitSet = new BitSet();
+        for (ParkingFloor parkingFloor : parkingFloors) {
+            bitSet.set(index++, parkingFloor.isFloorFull());
+        }
+        return bitSet.cardinality() == bitSet.size();
+    }
+
+    public boolean canPark(VehicleType vehicleType, boolean handicapped) {
+        for (ParkingFloor parkingFloor : parkingFloors) {
+            if (parkingFloor.canPark(vehicleType, handicapped))
+                return true;
+        }
+        return false;
+    }
+
 }
