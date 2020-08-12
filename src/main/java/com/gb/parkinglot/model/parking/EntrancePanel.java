32396e6449c79eaef8f08d68022abee4e757a9b3
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-13 02:42:21 +0530

    added logic to get parking ticket

--- src/main/java/com/gb/parkinglot/model/parking/EntrancePanel.java
+++ src/main/java/com/gb/parkinglot/model/parking/EntrancePanel.java
@@ -1,6 +1,10 @@
 package com.gb.parkinglot.model.parking;
 
+import com.gb.parkinglot.model.vehicle.Vehicle;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 public class EntrancePanel {
@@ -12,5 +16,27 @@ public class EntrancePanel {
 
     public ParkingTicket printTicket() {
         return null;
+    }
+
+    public ParkingTicket getParkingTicket(Vehicle vehicle, boolean handicapped) {
+        if (ParkingLot.INSTANCE.isFull())
+            return null;
+
+        for (ParkingFloor parkingFloor : ParkingLot.INSTANCE.getParkingFloors()) {
+            ParkingSpot parkingSpot = parkingFloor.getSpot(vehicle, handicapped);
+            if (parkingSpot != null) {
+                vehicle.setTicket(getParkingTicket());
+                return vehicle.getTicket();
+            }
+        }
+        return null;
+    }
+
+    private ParkingTicket getParkingTicket() {
+        ParkingTicket parkingTicket = new ParkingTicket();
+        parkingTicket.setIssuedAt(LocalDateTime.now());
+        parkingTicket.setTicketNumber(UUID.randomUUID().toString());
+        parkingTicket.setTicketStatus(TicketStatus.ACTIVE);
+        return parkingTicket;
     }
 }
