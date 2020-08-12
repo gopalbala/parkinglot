cab1c842bf9d21c1ed11418e21a81d2fdc9c26fd
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-13 02:42:57 +0530

    added logic to add panel and other details

--- src/main/java/com/gb/parkinglot/model/account/Admin.java
+++ src/main/java/com/gb/parkinglot/model/account/Admin.java
@@ -1,14 +1,26 @@
 package com.gb.parkinglot.model.account;
 
-import com.gb.parkinglot.model.parking.*;
+import com.gb.parkinglot.exceptions.InvalidParkingLotException;
+import com.gb.parkinglot.exceptions.InvlaidParkingFloorException;
+import com.gb.parkinglot.model.parking.EntrancePanel;
+import com.gb.parkinglot.model.parking.ExitPanel;
+import com.gb.parkinglot.model.parking.ParkingFloor;
+import com.gb.parkinglot.model.parking.ParkingSpot;
+import com.gb.parkinglot.repository.ParkingLotRepository;
 
 public class Admin extends Account {
-    void addParkingFloor(ParkingLot parkingLot, ParkingFloor parkingFloor) {
+    ParkingLotRepository parkingLotRepository = new ParkingLotRepository();
 
+    void addParkingFloor(String parkingLotId, ParkingFloor parkingFloor)
+            throws InvalidParkingLotException {
+        parkingLotRepository.addParkingFloor(parkingLotId,
+                parkingFloor);
     }
 
-    void addParkingSpot(ParkingFloor parkingFloor, ParkingSpot parkingSpot) {
-
+    void addParkingSpot(String parkingLotId,
+                        String parkingFloorId, ParkingSpot parkingSpot)
+            throws InvlaidParkingFloorException, InvalidParkingLotException {
+        parkingLotRepository.addParkingSpot(parkingLotId, parkingFloorId, parkingSpot);
     }
 
     void addEntrancePanel(EntrancePanel entrancePanel) {
