76088e3457029e46d18a45ddfee84272d947cd0c
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-15 10:54:32 +0530

    added code to assign and remove spot

--- src/main/java/com/gb/parkinglot/model/parking/ParkingSpot.java
+++ src/main/java/com/gb/parkinglot/model/parking/ParkingSpot.java
@@ -9,4 +9,14 @@ public abstract class ParkingSpot {
     private String parkingSpotId;
     private boolean isFree;
     private ParkingSpotType parkingSpotType;
+    private String assignedVehicleId;
+
+    public void assignVehicleToSpot(String vehicleId) {
+        this.assignedVehicleId = vehicleId;
+    }
+
+    public void freeSpot() {
+        this.isFree = true;
+        this.assignedVehicleId = null;
+    }
 }
